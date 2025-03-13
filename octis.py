bc18e29348e011de76acc59ef99fe65a389ec357
Kolala <kolala08@outlook.fr>
2025-03-13 17:45:04 +0000

    docstring + doctest

--- octis.py
+++ octis.py
@@ -4,57 +4,105 @@ Bloc = ((True, False, True),
 blocI = ((True,),
          (True,))
          
-BLOC = ((True, True),
+carre = ((True, True),
         (True, True))
 
-def gen_grille(w,h):
-  return[[False for i in range(w)] for j in range(h)]
+def gen_grille(largeur,hauteur):
+  """
+  largeur -- la largeur de la grille
+  hauteur -- la hauetur de la grille
+
+  retourne une liste de listes de False de largeur et hauteur donnees
+  """
+  return[[False for _ in range(largeur)] for _ in range(hauteur)]
 
 plateau=gen_grille(8,12)
 
 def tourne(bloc, ccw=True):
+  """
+  bloc -- une liste de listes (ou tuple de tuples) de booleens
+  ccw -- booleen, sens de la rotation, horaire par defaut
+
+  retourne une liste de liste du bloc tourne, avec les dimensions adaptees
+
+  >>> tourne(((True, False), (False,True)))
+  [[False, True], [True, False]]
+  >>> tourne(((True, True, True),), ccw=False)
+  [[True], [True], [True]]
+  """
   resultat=[]
-  if ccw:
-    for i in range(len(bloc[0])):
-      ligne=[]
-      for j in range(len(bloc)-1,-1,-1):
-        ligne.append(bloc[j][i])
-      resultat.append(ligne)
-  else:
-    for i in range(len(bloc[0])):
-      ligne=[]
-      for j in range(len(bloc)):
-        ligne.append(bloc[j][i])
-      resultat.append(ligne)
+  for i in range(len(bloc[0])):
+    ligne = []    
+    for j in range(-ccw, len(bloc) - 2 * len(bloc) * ccw - ccw, 1 - 2 * ccw):
+      ligne.append(bloc[j][i])
+    resultat.append(ligne)
   return resultat
   
-def affiche(bloc):
-  for B in bloc:
-    for b in B:
-      print("#" if b else " ", end=" ")
-    print("")
-  for i in range(len(bloc[0])):
-    print(i+1,end=" ")
-  print("")
+def affiche(bloc, numeros=True):
+  """
+  bloc -- une liste de listes de booleens
+  numeros -- un booleen
 
-def nb_blocs(bloc):
-  n=0
-  for B in bloc:
-    for b in B:
-      if b:
-        n+=1
-  return n
+  affiche le bloc dans la console avec les nombres en abscisse si demande
+  """
+  for ligne in bloc:
+    for case in ligne:
+      print("#" if case else " ", end=" ")
+    print("")
+  if numeros:
+    for i in range(len(bloc[0])):
+      print(i+1, end=" ")
+    print("")
+
+def compte_blocs(bloc):
+  """
+  bloc -- une liste de listes de booleens
+
+  renvoie le nombre de True dans le bloc
+
+  >>> compte_blocs(((True, False), (False, True)))
+  2
+  >>> compte_blocs(((False,),(False,)))
+  0
+  """
+  nombre=0
+  for ligne in bloc:
+    for case in ligne:
+      if case:
+        nombre += 1
+  return nombre
   
 def copie_grille(grille):
-  return [[elt for elt in ligne] for ligne in grille]
+  """
+  grille -- une liste de listes de booleens
+  
+  renvoie une copie de la grille
+  >>> copie_grille(((True, False), (False, True)))
+  [[True, False], [False, True]]
+  """
+  return [[case for case in ligne] for ligne in grille]
 
 def copie_cases(plateau, bloc, offsetX, offsetY):
+  """
+  plateau -- une liste de listes de booleens
+  bloc -- une liste de listes de booleens
+  offsetX -- un entier
+  offsetY -- un entier
+
+  modifie plateau en ajoutant le bloc (seulement True) sur le plateau avec un decalage a partir du bas gauche du bloc
+  """
   for i in range(len(bloc)):
     for j in range(len(bloc[0])):
       if bloc[i][j]:
         plateau[offsetY + i][offsetX + j] = bloc[i][j]
 
 def supp_lignes(plateau, indices_lignes):
+  """
+  plateau -- une liste de listes de booleens
+  indice_lignes -- une liste d'indices des lignes a supprimer
+
+  supprime les lignes aux indices donnes et fait descendre les lignes dessus
+  """
   vide = [False for _ in plateau[0]]
   indices_lignes.sort()
   for indice in indices_lignes:
@@ -62,52 +110,66 @@ def supp_lignes(plateau, indices_lignes):
     for i in range(indice, 0, -1):
       plateau[i] = plateau[i-1]
 
-def lignes_finies(plateau):
-  lignes_sup = []
-  for i,L in enumerate(plateau):
-    if not False in L:
-      lignes_sup.append(i)
-  supp_lignes(plateau, lignes_sup)
+def detect_lignes(plateau):
+  """
+  plateau -- une liste de listes de booleens
 
-def titouan(plateau):
-  for i in range(len(plateau)):
-    if not False in plateau[0]:
-      liste = [[False for _ in range(len(plateau[0]))]]
-      liste.append([[x for x in plateau[k]] for k in range(i)])
-      for j in range(i+1):
-        plateau[j] = liste[j]
+  detecte et renvoie une liste des indices de toutes les lignes completees sur le plateau
+  """
+  lignes_completes = []
+  for indice, ligne in enumerate(plateau):
+    if not False in ligne:
+      lignes_completes.append(indice)
+  return lignes_completes
+
+def lignes_finies(plateau):
+  """
+  plateau -- une liste de listes de booleens
+
+  supprime les lignes finies du plateau et fait descendre les lignes dessus
+  """
+  lignes_completes = detect_lignes(plateau)
+  supp_lignes(plateau, lignes_completes)
 
 def tombe(plateau, bloc, x):
   """
   plateau -- le plateau a modifier, a effet de bord
   bloc -- le bloc a pose
   x -- la position x ou poser le bloc, concerne le bout le plus a gauche
+
+  fait tomber un bloc sur le plateau avec un abscisse donne
   """
-  # Changer en une seule verification
-  x=min(x, len(plateau[0])-len(bloc[0]))
-  nb_apres=nb_blocs(bloc)+nb_blocs(plateau)
-  hauteur=0
-  copie=[]
+  # Changer en une seule verification avant la fonction
+  x=min(x, len(plateau[0]) - len(bloc[0]))
+  x=max(0, x)
+
+  total_blocs = compte_blocs(bloc) + compte_blocs(plateau)
+  hauteur = 0
+  copie = []
   while hauteur + len(bloc) <= len(plateau):
-    copie=copie_grille(plateau)
+    copie = copie_grille(plateau)
     copie_cases(copie, bloc, x, hauteur)
-    if nb_apres != nb_blocs(copie):
-      if hauteur-1 < 0:
-        print("Impossible de placer")
-        return
-      copie_cases(plateau, bloc, x, hauteur-1)
+    if total_blocs != compte_blocs(copie):
+      if hauteur - 1 < 0:
+        return -1
+      copie_cases(plateau, bloc, x, hauteur - 1)
       return
     hauteur += 1
-  copie_cases(plateau, bloc, x, hauteur-1)
+  copie_cases(plateau, bloc, x, hauteur - 1)
   lignes_finies(plateau)
 
 
-tombe(plateau, BLOC, 0)
-tombe(plateau, blocI, 1)
-tombe(plateau, BLOC, 2)
-#tombe(plateau, BLOC, 4)
-tombe(plateau, BLOC, 6)
-#tombe(plateau, tourne(tourne(Bloc)), 1)
-#tombe(plateau, tourne(tourne(Bloc)), 4)
-#tombe(plateau, blocI, 7)
-affiche(plateau)
+def main():
+  tombe(plateau, carre, 0)
+  tombe(plateau, blocI, 1)
+  tombe(plateau, carre, 2)
+  tombe(plateau, carre, 6)
+  affiche(plateau)
+
+if __name__ == "__main__":
+    # On teste toutes les fonctions avant de commencer
+    import doctest
+    doctest.testmod(verbose=False)
+
+    # On appelle la fonction principale
+    main()
